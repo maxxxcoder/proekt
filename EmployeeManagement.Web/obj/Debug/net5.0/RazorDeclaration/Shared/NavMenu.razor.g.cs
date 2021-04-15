a411ddbb0b0aaaa499b3086079e28660d63f8a3d
Maksim Tkatsenko <mtkatsenko@gmail.com>
2021-04-15 23:51:10 +0300

    Video 6

--- EmployeeManagement.Web/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ EmployeeManagement.Web/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -90,7 +90,7 @@ using EmployeeManagement.Web.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 28 "C:\Users\mtkat\source\repos\BlazorTutorial\EmployeeManagement.Web\Shared\NavMenu.razor"
+#line 18 "C:\Users\mtkat\source\repos\BlazorTutorial\EmployeeManagement.Web\Shared\NavMenu.razor"
        
     private bool collapseNavMenu = true;
 
