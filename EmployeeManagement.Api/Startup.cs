73474163cf751893da8789ab9b3bcf2aebc2d76a
Maksim Tkatsenko <mtkatsenko@gmail.com>
2021-05-19 20:32:56 +0300

    Video 12

--- EmployeeManagement.Api/Startup.cs
+++ EmployeeManagement.Api/Startup.cs
@@ -1,7 +1,9 @@
+using EmployeeManagement.Api.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,7 +28,8 @@ namespace EmployeeManagement.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DBConnection")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
