c77282a61bcaf0eb6b737d86f926f5016a788479
Maksim Tkatsenko <mtkatsenko@gmail.com>
2021-05-25 15:20:31 +0300

    Video 36

--- EmployeeManagement.Models/Employee.cs
+++ EmployeeManagement.Models/Employee.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EmployeeManagement.Models.CustomValidators;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
@@ -13,6 +14,8 @@ namespace EmployeeManagement.Models
         public string FirstName { get; set; }
         [Required]
         public string LastName { get; set; }
+        [EmailAddress]
+        [EmailDomainValidator(AllowedDomain = "pragimtech.com")]
         public string Email { get; set; }
         public DateTime DateOfBrith { get; set; }
         public Gender Gender { get; set; }
